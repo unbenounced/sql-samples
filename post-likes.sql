d75574d2e3c56b0a20ca724fe31c98c157f72a0a
eric <eric@unbenounced.com>
2024-05-24 23:48:40 -0400

    avg

---
+++ post-likes.sql
@@ -0,0 +1,11 @@
+/*
+You are given a list of posts of a Facebook user. Find the average number of likes.
+*/
+
+--eric 
+SELECT AVG(no_of_likes)
+  FROM fb_posts;
+
+--strata
+SELECT AVG(no_of_likes)
+FROM fb_posts
